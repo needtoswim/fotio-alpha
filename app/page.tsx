a789c73e31113de9a2a2bbaeb8b6541ee4a74f99
john <lyonsalt@gmail.com>
2024-12-10 18:31:02 +1100

    Fix ESlint issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import Link from 'next/link';
+// import Image from "next/image";
+// import Link from 'next/link';
 import './globals.css'
 import classes from "./page.module.css"
 import Backdrop from './ui/backdrop/Backdrop'
